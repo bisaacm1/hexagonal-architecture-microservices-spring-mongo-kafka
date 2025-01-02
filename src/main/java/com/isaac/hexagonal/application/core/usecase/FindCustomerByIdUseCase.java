b5f404797dba9d77c88475e3962b760b5c30f883
Isaac Batista <isaac.maciel@capgemini.com>
2025-01-02 14:14:56 -0300

    CRUD read

---
+++ src/main/java/com/isaac/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
@@ -0,0 +1,21 @@
+package com.isaac.hexagonal.application.core.usecase;
+
+import com.isaac.hexagonal.application.core.domain.Customer;
+import com.isaac.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.isaac.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+
+public class FindCustomerByIdUseCase implements FindCustomerByIdInputPort {
+
+    private final FindCustomerByIdOutputPort findCustomerByIdOutputPort;
+
+    public FindCustomerByIdUseCase(FindCustomerByIdOutputPort findCustomerByIdOutputPort) {
+        this.findCustomerByIdOutputPort = findCustomerByIdOutputPort;
+    }
+
+    @Override
+    public Customer find(String id) {
+        return findCustomerByIdOutputPort.find(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+
+}
